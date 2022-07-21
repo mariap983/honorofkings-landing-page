92a2cf3370aed6243a576f0f851743d0d74f5c1d
mariap983 <maria.petrovaa983@gmail.com>
2022-07-21 14:31:43 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/game-landing-page/",
-  plugins: [react()],
+  plugins: [react()]
 })
